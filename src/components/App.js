a00bf8b6a31d0fa8e976fe223edb43a0f2be9236
Sathish-design-1 <sathishb2310@gmail.com>
2026-01-30 16:10:28 +0530

    Complete User Info Redux implementation and fix Node version issues

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,38 @@
-
-import React from "react";
-import './../styles/App.css';
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { updateName, updateEmail } from '../actions';
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+  const { name, email } = useSelector((state) => state);
+  const dispatch = useDispatch();
 
-export default App
+  return (
+    <div id="main">
+      <h1>User Information</h1>
+      
+      <div className="input-section">
+        <label>Name: </label>
+        <input 
+          type="text" 
+          value={name} 
+          onChange={(e) => dispatch(updateName(e.target.value))} 
+        />
+        <br />
+        <label>Email: </label>
+        <input 
+          type="email" 
+          value={email} 
+          onChange={(e) => dispatch(updateEmail(e.target.value))} 
+        />
+      </div>
+
+      <div className="output">
+        <p>Current values in store:</p>
+        <p>Name - {name}</p>
+        <p>Email - {email}</p>
+      </div>
+    </div>
+  );
+};
+
+export default App;
